9a1a6bfe3e11ae274f563feaf381238809291e85
Qipeax <qipeax@gmail.com>
2024-12-19 18:46:34 +0300

    'deploy'

---
+++ Semestr.js
@@ -0,0 +1,63 @@
+const newTableValues = [
+    "Базовое бэкенд-приложение",
+    " HTTP-запросы4",
+    "JSON и работа с ним",
+    "HTTP-ответы",
+    "Проектирование API",
+    "Роутинг и его настройка",
+    "NoSQL базы данных",
+    "Обеспечение авторизации и доступа пользователей",
+    "Работа сторонних сервисов уведомления и авторизации",
+    "Основы ReactJS",
+    "Работа с компонентами динамической DOM",
+    "Использование хуков в React",
+    "Основы микросервисной архитектуры",
+    "Разработка классических модулей веб-приложений",
+    "Разработка классических модулей веб-приложений", 
+];
+const columnIndex = 1;
+
+function movingtitle() {
+    const table = document.getElementById('lection');
+    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+
+    for (let i = 0; i < rows.length; i++) {
+        const cells = rows[i].getElementsByTagName('td');
+       if(cells.length > columnIndex && i < newTableValues.length)
+       {
+          cells[columnIndex].textContent = newTableValues[i];
+       }
+  }
+}
+
+const h1change = document.getElementById('h1alert');
+ 
+h1change.addEventListener('click', function() {
+    alert('Вы кликнули. Ну молодэц ваще!');
+    console.log("Пизда нихуя не работает");
+    
+});
+const doubleclick = document.getElementById('megaclick');
+
+doubleclick.addEventListener('dblclick', function(){
+    alert('Что ты ещё хочешь тут найти?');
+});
+
+function showlection() {
+    element = document.querySelector('.lections');
+    element.style.visibility = 'visible';
+}
+function hidelection() {
+    element = document.querySelector('.lections');
+    element.style.visibility = 'hidden';
+}
+
+function sendcontact(){
+    alert('Ваша форма была отправлена!')
+    var colorlol = document.getElementsByTagName("contactcolor")[0];
+    colorlol.style.backgroundColor = "#ffffff";
+}
+document.querySelector('button').addEventListener('click', function(){
+    document.querySelector('p').style.color='green';
+})
